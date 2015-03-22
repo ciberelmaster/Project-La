2065beee2459f749bd50c5c8960b690a44216784
CIBERELMASTER <chamoandvicko@gmail.com>
2015-03-21 21:17:25 -0500

    Create jumbotron.php

---
+++ jumbotron.php
@@ -0,0 +1,30 @@
+<?
+  function jumbotron() {
+?>
+<html>
+  <div class="nav nav-tabs">
+      <div class="container">
+        <ul class="pull-left">
+          <li><span>PROJECT-La</span></li>
+          <li><a href="index.php">HOME</a></li>
+        </ul>
+        
+        <ul class="pull-right">
+          <li><a href="signup.php">SIGN UP</a></li>
+          <li><a href="login.php">LOGIN</a></li>
+          <li><a href="about.php">ABOUT</a></li>
+        </ul>
+      </div>
+    </div>
+    
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Project-La</h1>
+        <p>Donde la gente con sus temas viene aqui a compartirlos, Que esperas!</p>
+        <a href="singup.html">Registrate</a>
+      </div>
+    </div>
+</html>
+<?
+  }
+?>
