9897bfd131724ee834d9417e1d991dce6db02a16
CIBERELMASTER <chamoandvicko@gmail.com>
2015-03-21 21:19:38 -0500

    Update jumbotron.php

--- jumbotron.php
+++ jumbotron.php
@@ -2,6 +2,12 @@
   function jumbotron() {
 ?>
 <html>
+  <head>
+    <link type="text/css" rel="stylesheet" href="main.css">
+    <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css" rel="stylesheet">
+    <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
+  </head>
+<body>
   <div class="nav nav-tabs">
       <div class="container">
         <ul class="pull-left">
@@ -24,6 +30,7 @@
         <a href="singup.html">Registrate</a>
       </div>
     </div>
+  </body>
 </html>
 <?
   }
