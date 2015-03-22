75458b19591cb4a8259d43a87d1c6dc5564676c5
CIBERELMASTER <chamoandvicko@gmail.com>
2015-03-21 21:22:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<!DOCTYPE HTML>
+<?
+  include('jumbotron.php');
+?>
 <html>
   <head>
     <title>La.com</title>
@@ -6,31 +8,12 @@
     <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css" rel="stylesheet">
     <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
   </head>
-  
+</html>
+<?
+  jumbotron();
+?>
+<html>
   <body>
-    <div class="nav nav-tabs">
-      <div class="container">
-        <ul class="pull-left">
-          <li><span>PROJECT-La</span></li>
-          <li><a href="index.php">HOME</a></li>
-        </ul>
-        
-        <ul class="pull-right">
-          <li><a href="signup.php">SIGN UP</a></li>
-          <li><a href="login.php">LOGIN</a></li>
-          <li><a href="about.php">ABOUT</a></li>
-        </ul>
-      </div>
-    </div>
-    
-    <div class="jumbotron">
-      <div class="container">
-        <h1>Project-La</h1>
-        <p>Donde la gente con sus temas viene aqui a compartirlos, Que esperas!</p>
-        <a href="singup.html">Registrate</a>
-      </div>
-    </div>
-    
     <div class="neighborhood-guides">
       <div class="container">
         <b><h2>Lo que hay en La.</h2></b>
@@ -101,4 +84,5 @@
       </address>
   </center>
   </body>
+</html>
 </html>
