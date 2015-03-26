1cf00e730973c581441a8df3d407fe31669ea00c
CIBERELMASTER <chamoandvicko@gmail.com>
2015-03-25 19:55:34 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,6 +3,9 @@
 ?>
 <html>
 <head>
+  <link type="text/css" rel="stylesheet" href="main.css">
+  <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/bootstrap.css" rel="stylesheet">
+  <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
 </head>
 <body>
   <div class="container">
