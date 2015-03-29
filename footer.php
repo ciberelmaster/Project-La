5b31ace2268019b0ea0844ca4530068a745243d7
CIBERELMASTER <chamoandvicko@gmail.com>
2015-03-28 23:44:46 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -11,7 +11,22 @@
   <div class="container">
     <div class="site-footer" role="contentinfo">
       <ul class="site-footer-links right">
-        
+        <li><a href="about.php" data-ga-click="Footer, go to about, text:about">About</a></li>
+        <li><a href="api.php" data-ga-click="Footer, go to api, text:api">API</a></li>
+        <li><a href="blog.php" data-ga-click="Footer, go to blog, text:blog">Blog</a></li>
+        <li><a href="Enterprise.php" data-ga-click="Footer, go to La, text:La">La</a></li>
+      </ul>
+
+      <a href="index.php" aria-label="Homepage">
+        <span><img src="lalogo.jpgeg"</span>
+      </a>
+    
+      <ul class="site-footer-links">
+        <li>&copy; 2015 <span title="0.06882s from github-fe126-cp1-prd.iad.github.net">Project-la</span>, Co.</li>
+          <li><a href="terminos.php" data-ga-click="Footer, go to terms, text:terms">Terms</a></li>
+          <li><a href="Privacidad.php" data-ga-click="Footer, go to privacy, text:privacy">Privacy</a></li>
+          <li><a href="seguridad.php" data-ga-click="Footer, go to security, text:security">Security</a></li>
+          <li><a href="contac.php" data-ga-click="Footer, go to contact, text:contact">Contact</a></li>
       </ul>
     </div>
   </div>
